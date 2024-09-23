d9d4957d4bade7508cecdf99d6b48c19b1b2993f
JYPJUN <psw9502@naver.com>
2024-09-23 17:54:30 +0900

    Feat: 회원가입 기능 개발

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return <section></section>;
+};
+
+export default Signup;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import LoginInput from "../components/LoginInput";
+import { useState } from 'react';
+import LoginInput from '../components/LoginInput';
 
-import "./Login.css";
+import './Login.sass';
 
 const LoginPage = () => {
   const [exp, setExp] = useState(50);
+
   return (
-    <div className="Login">
-      <section className="MainLogo"> {/*} 로고 이미지 넣는 곳 {*/}
-        <img className="Logoimg" src="/gamethumbnail/favicon.png" alt="Logo" />
+    <div className="login-Page">
+      <section className="login-Logo">
+        {/*} 로고 이미지 넣는 곳 {*/}
+        <img className="login-Logo__img" src="/gamethumbnail/favicon.png" alt="Logo" />
       </section>
 
-      <section className="Input"> {/*} 아이디 비밀번호 입력 구역 {*/}
+      <section className="login-Input">
+        {/*} 아이디 비밀번호 입력 구역 {*/}
         <LoginInput />
       </section>
     </div>
