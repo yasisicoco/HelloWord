395c3d77c2134913e6a18970d640dd1f2b77c21d
jinwoo <yasisicoco@gmail.com>
2024-09-23 15:36:25 +0900

    Feat: 홈페이지 레이아웃

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
-      <h1>Hello Word!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
