c031d73c35cf21116e82543b7144bae6f10b5abf
박용빈 <pyb0718@naver.com>
2024-09-25 12:40:06 +0900

    Feat: wordService 프로젝트 생성

---
+++ backend/word-service/src/main/java/com/helloword/wordservice/WordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.helloword.wordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordServiceApplication.class, args);
+	}
+
+}
---
+++ backend/word-service/src/test/java/com/helloword/wordservice/WordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloword.wordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
