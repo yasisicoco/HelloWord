d65948c9fd81281673c1ff6e1dc16dc2f7070f1e
강세현 <gedfgrdt0@gmail.com>
2024-09-25 13:49:10 +0900

    Feat: probability service 모듈 추가

---
+++ backend/probability-service/src/main/java/org/helloword/probabilityservice/ProbabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.helloword.probabilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbabilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbabilityServiceApplication.class, args);
+	}
+
+}
