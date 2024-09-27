26c8b09e1f5b67fe86631430f7e3558e82c1342d
박용빈 <pyb0718@naver.com>
2024-09-27 14:50:18 +0900

    Feat: Word Service 어노테이션 추가  - DiscoveryClient  - FeignClients

--- backend/word-service/src/main/java/com/helloword/wordservice/WordServiceApplication.java
+++ backend/word-service/src/main/java/com/helloword/wordservice/WordServiceApplication.java
@@ -2,8 +2,12 @@ package com.helloword.wordservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class WordServiceApplication {
 
 	public static void main(String[] args) {
