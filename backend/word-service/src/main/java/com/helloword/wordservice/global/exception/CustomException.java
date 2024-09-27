0aec7fc2ee41b6402d44b138c5113515fc3340c4
박용빈 <pyb0718@naver.com>
2024-09-27 17:28:29 +0900

    Feat: INTERNAL_SERVER_ERROR 추가

--- backend/word-service/src/main/java/com/helloword/wordservice/global/exception/CustomException.java
+++ backend/word-service/src/main/java/com/helloword/wordservice/global/exception/CustomException.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CustomException {
 
-    NOT_FOUND(404, "리소스를 찾을 수 없습니다");
+    NOT_FOUND(404, "리소스를 찾을 수 없습니다"),
+
+    INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다");
 
     private int code;
     private String message;
