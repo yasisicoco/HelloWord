8b97cad1b0af8eb2a9b76bbfca9cfe174ee61cf8
박용빈 <pyb0718@naver.com>
2024-09-30 13:18:11 +0900

    Feat: Enum 추가

---
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/character/model/CharacterType.java
@@ -0,0 +1,8 @@
+package com.helloword.kidservice.domain.character.model;
+
+public enum CharacterType {
+    Hitori,
+    Nijika,
+    Ryo,
+    Ikuyo
+}
---
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/model/Gender.java
@@ -0,0 +1,6 @@
+package com.helloword.kidservice.domain.kid.model;
+
+public enum Gender {
+    M, F
+}
+
