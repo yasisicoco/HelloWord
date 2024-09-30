ea89c9503bdf4d54d3cf3644cbf2397ec9809f05
강세현 <gedfgrdt0@gmail.com>
2024-09-30 10:34:47 +0900

    Feat: CustomException 구조 수정 및 예외 추가

--- backend/probability-service/src/main/java/org/helloword/probabilityservice/global/exception/CustomException.java
+++ backend/probability-service/src/main/java/org/helloword/probabilityservice/global/exception/CustomException.java
@@ -7,9 +7,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CustomException {
 
-	ACCESS_DENIED_EXCEPTION(403,"AccessDeniedException","권한이 없습니다");
+	BAD_REQUEST(400, "잘못된 요청입니다"),
+	UNAUTHORIZED(401, "인증이 필요합니다"),
+	FORBIDDEN(403, "권한이 없습니다"),
+	NOT_FOUND(404, "리소스를 찾을 수 없습니다"),
+	METHOD_NOT_ALLOWED(405, "허용되지 않은 HTTP 메서드입니다"),
+	NOT_ACCEPTABLE(406, "허용되지 않는 요청입니다"),
+	CONFLICT(409, "충돌이 발생했습니다"),
+	UNSUPPORTED_MEDIA_TYPE(415, "지원하지 않는 미디어 타입입니다"),
+	INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다");
 
-	private int statusNum;
-	private String errorCode;
-	private String errorMessage;
+	private int code;
+	private String message;
 }
