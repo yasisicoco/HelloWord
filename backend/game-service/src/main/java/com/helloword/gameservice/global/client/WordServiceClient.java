6e2826c4c39afcc0339356d2385d69dc4361c5a7
강세현 <gedfgrdt0@gmail.com>
2024-10-01 17:20:49 +0900

    Feat: 짝맞추기 카드게임 관련 client 메소드 추가

--- backend/game-service/src/main/java/com/helloword/gameservice/global/client/WordServiceClient.java
+++ backend/game-service/src/main/java/com/helloword/gameservice/global/client/WordServiceClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.helloword.gameservice.domain.game.dto.response.PairWordResponseDto;
 import com.helloword.gameservice.domain.game.dto.response.SpeechWordResponseDto;
 import com.helloword.gameservice.domain.game.dto.response.SpeedWordResponseDto;
 
@@ -15,4 +16,7 @@ public interface WordServiceClient {
 
 	@GetMapping("/api/words/internal/speech")
 	SpeechWordResponseDto getSpeechWords(@RequestParam("kidId") Long kidId);
+
+	@GetMapping("/api/words/internal/pair")
+	PairWordResponseDto getPairWords(@RequestParam("kidId") Long kidId);
 }
