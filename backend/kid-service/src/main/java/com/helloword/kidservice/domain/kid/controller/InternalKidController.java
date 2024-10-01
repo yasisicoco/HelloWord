6b2ebb4e1e869b148ece0c21a51800d440ba0004
박용빈 <pyb0718@naver.com>
2024-10-01 19:48:35 +0900

    Fix: 아이 경험치 추가 기능 수정

--- backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/controller/InternalKidController.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/controller/InternalKidController.java
@@ -4,21 +4,17 @@ import com.helloword.kidservice.domain.kid.dto.request.UpdateExpRequestDto;
 import com.helloword.kidservice.domain.kid.service.KidService;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Hidden
 @RestController
 @RequestMapping("/internal/kids")
 @RequiredArgsConstructor
 public class InternalKidController {
-    private static KidService kidService;
+    private final KidService kidService;
 
     @PatchMapping("/exp")
-    public void updateKidExp(@RequestBody UpdateExpRequestDto updateKidExpReuestDto) {
-        kidService.increaseExperience(updateKidExpReuestDto);
-
+        public void updateKidExp(@ModelAttribute UpdateExpRequestDto updateExpRequestDto) {
+        kidService.increaseExperience(updateExpRequestDto);
     }
 }
