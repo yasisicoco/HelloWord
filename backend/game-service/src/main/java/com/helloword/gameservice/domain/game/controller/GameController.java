4339dffe30aa2702b32afb12a0c3d649e24abbb7
강세현 <gedfgrdt0@gmail.com>
2024-10-01 17:00:20 +0900

    Feat: 스피드 카드게임 관련 Controller API 메서드 추가

--- backend/game-service/src/main/java/com/helloword/gameservice/domain/game/controller/GameController.java
+++ backend/game-service/src/main/java/com/helloword/gameservice/domain/game/controller/GameController.java
@@ -1,4 +1,26 @@
 package com.helloword.gameservice.domain.game.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helloword.gameservice.domain.game.dto.response.SpeedGameResponseDto;
+import com.helloword.gameservice.domain.game.service.GameService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/games")
+@Slf4j
 public class GameController {
+
+	private final GameService gameService;
+
+	@GetMapping("/speed-cards")
+	public SpeedGameResponseDto getSpeedGameCards(@RequestParam("kidId") Long kidId) {
+		return gameService.getSpeedGameCards(kidId);
+	}
 }
