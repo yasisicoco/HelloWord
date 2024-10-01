7cdf1b12f488f382d8ef68dd845169de4ab04ff9
박용빈 <pyb0718@naver.com>
2024-10-01 19:23:37 +0900

    feat: kid api 기능 추가  - S3 테스트용 upload api 삭제  - 아이 등록 기능 추가  - 아이 조회 기능 추가  - 아이 목록 조회 기능 추가  - 아이 수정 기능 추가  - 캐릭터 변경 기능 추가  - 아이 경험치 획득 기능 추가  - 아이 삭제 기능 추가

--- backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/service/KidServiceImpl.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/service/KidServiceImpl.java
@@ -1,20 +1,127 @@
 package com.helloword.kidservice.domain.kid.service;
 
+import com.helloword.kidservice.domain.kid.dto.request.CraeteKidRequestDto;
+import com.helloword.kidservice.domain.kid.dto.request.UpdateExpRequestDto;
+import com.helloword.kidservice.domain.kid.dto.request.UpdateKidRequestDto;
+import com.helloword.kidservice.domain.kid.dto.request.UpdateMainCharactorRequestDto;
+import com.helloword.kidservice.domain.kid.dto.response.KidResponseDto;
+import com.helloword.kidservice.domain.kid.model.Kid;
+import com.helloword.kidservice.domain.kid.repository.KidRepository;
+import com.helloword.kidservice.global.exception.MainException;
 import com.helloword.kidservice.global.utils.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.helloword.kidservice.global.exception.CustomException.KID_FORBIDDEN;
+import static com.helloword.kidservice.global.exception.CustomException.KID_NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class KidServiceImpl implements KidService {
 
     private final FileService fileService;
+    private final KidRepository kidRepository;
+
+    @Transactional
+    @Override
+    public KidResponseDto createKid(Long userId, MultipartFile profileImage, CraeteKidRequestDto craeteKidRequestDto) {
+
+        String profileImageUrl = !profileImage.isEmpty()
+                ? fileService.uploadImage(profileImage)
+                : null;
+
+        Kid kid = Kid.createKid(
+                userId,
+                craeteKidRequestDto.name(),
+                craeteKidRequestDto.birthDate(),
+                craeteKidRequestDto.gender(),
+                profileImageUrl
+        );
+        kidRepository.save(kid);
+
+        return new KidResponseDto(kid);
+    }
 
     @Override
-    public String uploadKidImage(MultipartFile file) {
-        return fileService.uploadImage(file);
+    public List<KidResponseDto> getKidsByUserId(Long userId) {
+        return  kidRepository.findByUserId(userId)
+                .stream()
+                .map(KidResponseDto::new)  // Kid -> KidResponseDto 변환
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public KidResponseDto getKid(Long userId, Long kidId) {
+        Kid kid = findKidById(kidId);
+        checkOwnership(kid.getUserId(), userId);
+        return new KidResponseDto(kid);
+    }
+
+    @Transactional
+    @Override
+    public KidResponseDto updateKid(Long userId, Long kidId, MultipartFile profileImage, UpdateKidRequestDto updateKidRequestDto) {
+        Kid kid = findKidById(kidId);
+        checkOwnership(kid.getUserId(), userId);
+
+        String profileImageUrl = !profileImage.isEmpty()
+                ? fileService.uploadImage(profileImage)
+                : null;
+
+        kid.updateKidInfo(
+                updateKidRequestDto.name(),
+                updateKidRequestDto.birthDate(),
+                updateKidRequestDto.gender(),
+                profileImageUrl
+        );
+
+        return new KidResponseDto(kid);
+    }
+
+    @Transactional
+    @Override
+    public KidResponseDto updateMainCharacter(Long userId, Long kidId, UpdateMainCharactorRequestDto updateMainCharactorRequestDto) {
+        Kid kid = findKidById(kidId);
+        checkOwnership(kid.getUserId(), userId);
+
+        kid.changeMainCharacter(updateMainCharactorRequestDto.mainCharacter());
+
+        return new KidResponseDto(kid);
+    }
+
+    @Transactional
+    @Override
+    public void deleteKid(Long userId, Long kidId) {
+        Kid kid = findKidById(kidId);
+        checkOwnership(kid.getUserId(), userId);
+        kidRepository.delete(kid);
+    }
+
+    @Transactional
+    @Override
+    public void increaseExperience(UpdateExpRequestDto updateExpRequestDto) {
+        Kid kid = findKidById(updateExpRequestDto.kidId());
+
+        kid.addExperience(updateExpRequestDto.exp());
+
+        kidRepository.save(kid);
+    }
+
+    private Kid findKidById(Long kidId) {
+        return kidRepository.findById(kidId)
+                .orElseThrow(() -> new MainException(KID_NOT_FOUND));
+    }
+
+    private void checkOwnership(Long ownerId, Long requestUserId) {
+        if (!ownerId.equals(requestUserId)) {
+            throw new MainException(KID_FORBIDDEN);
+        }
     }
 }
