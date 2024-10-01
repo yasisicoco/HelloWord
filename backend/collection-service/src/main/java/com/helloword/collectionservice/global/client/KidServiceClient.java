0ebfeb869a3d409d7d40bcab5500f771ccaa4e54
강세현 <gedfgrdt0@gmail.com>
2024-10-01 11:27:10 +0900

    Feat: KidServiceClient 추가 및 exp 수정 메소드 추가

---
+++ backend/collection-service/src/main/java/com/helloword/collectionservice/global/client/KidServiceClient.java
@@ -0,0 +1,14 @@
+package com.helloword.collectionservice.global.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.helloword.collectionservice.domain.collection.dto.request.KidExpUpdateRequestDto;
+
+@FeignClient(name = "kid-service")
+public interface KidServiceClient {
+
+	@PatchMapping("/api/kids/internal")
+	void updateKidExp(@RequestBody KidExpUpdateRequestDto requestDto);
+}
