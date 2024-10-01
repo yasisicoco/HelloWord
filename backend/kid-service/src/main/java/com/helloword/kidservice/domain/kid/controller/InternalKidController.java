1d05301d52dc3161e17ba45d0ad75b40010b44db
박용빈 <pyb0718@naver.com>
2024-10-01 19:57:03 +0900

    Feat: Swagger 적용

--- backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/controller/InternalKidController.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/controller/InternalKidController.java
@@ -3,18 +3,21 @@ package com.helloword.kidservice.domain.kid.controller;
 import com.helloword.kidservice.domain.kid.dto.request.UpdateExpRequestDto;
 import com.helloword.kidservice.domain.kid.service.KidService;
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@Hidden
 @RestController
 @RequestMapping("/internal/kids")
 @RequiredArgsConstructor
+@Tag(name = "[내부]아이 API", description = "서버 내부에서 통신하는 아이에 대한 API 입니다.")
 public class InternalKidController {
     private final KidService kidService;
 
     @PatchMapping("/exp")
-        public void updateKidExp(@ModelAttribute UpdateExpRequestDto updateExpRequestDto) {
+    @Operation(summary = "아이 경험치 수정", description = "요청한 정보로 아이의 경험치를 수정합니다.")
+    public void updateKidExp(@ModelAttribute UpdateExpRequestDto updateExpRequestDto) {
         kidService.increaseExperience(updateExpRequestDto);
     }
 }
