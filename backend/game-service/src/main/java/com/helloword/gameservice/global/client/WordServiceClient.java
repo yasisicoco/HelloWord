b97a5a2365be9e0dd4df972717dcd0c34ddfd6b7
강세현 <gedfgrdt0@gmail.com>
2024-10-01 17:13:07 +0900

    Feat: 말하기 카드게임 관련 Client 메서드 추가

--- backend/game-service/src/main/java/com/helloword/gameservice/global/client/WordServiceClient.java
+++ backend/game-service/src/main/java/com/helloword/gameservice/global/client/WordServiceClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.helloword.gameservice.domain.game.dto.response.SpeechWordResponseDto;
 import com.helloword.gameservice.domain.game.dto.response.SpeedWordResponseDto;
 
 @FeignClient(name = "word-service")
@@ -12,4 +13,6 @@ public interface WordServiceClient {
 	@GetMapping("/api/words/internal/speed")
 	SpeedWordResponseDto getSpeedWords(@RequestParam("kidId") Long kidId);
 
+	@GetMapping("/api/words/internal/speech")
+	SpeechWordResponseDto getSpeechWords(@RequestParam("kidId") Long kidId);
 }
