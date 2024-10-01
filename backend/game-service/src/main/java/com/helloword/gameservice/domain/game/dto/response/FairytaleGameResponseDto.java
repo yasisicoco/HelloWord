034e6a600d8539ffe62f55598ec56f92b49dcd96
강세현 <gedfgrdt0@gmail.com>
2024-10-01 17:42:17 +0900

    Feat: 동화 단어넣기 카드 게임 관련 dto 추가

---
+++ backend/game-service/src/main/java/com/helloword/gameservice/domain/game/dto/response/FairytaleGameResponseDto.java
@@ -0,0 +1,32 @@
+package com.helloword.gameservice.domain.game.dto.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FairytaleGameResponseDto {
+	private String storyTitle;
+	private int sentenceCount;
+	private List<RoundDto> rounds;
+
+	@Getter
+	@AllArgsConstructor
+	public static class RoundDto {
+		private String sentence;
+		private String imageUrl;
+		private int sentenceOrder;
+		private WordDto correctWord;
+		private List<WordDto> incorrectWords;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	public static class WordDto {
+		private Long wordId;
+		private String word;
+	}
+}
+
