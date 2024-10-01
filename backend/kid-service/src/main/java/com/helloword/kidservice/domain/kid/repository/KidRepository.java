5f50d103924c23d0da65fb3fb783903c193a5934
박용빈 <pyb0718@naver.com>
2024-10-01 19:24:08 +0900

    feat: 아이 목록 조회 기능 추가

--- backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/repository/KidRepository.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/repository/KidRepository.java
@@ -4,6 +4,9 @@ import com.helloword.kidservice.domain.kid.model.Kid;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KidRepository extends JpaRepository<Kid, Long> {
+    List<Kid> findByUserId(Long userId);
 }
