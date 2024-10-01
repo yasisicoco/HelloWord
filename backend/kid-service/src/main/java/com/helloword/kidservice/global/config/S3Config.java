9fc330fb5fce7c2f3252e290b72c3dd4842e5e37
박용빈 <pyb0718@naver.com>
2024-10-01 19:28:12 +0900

    Refactor: S3 라이브러리 버전 업그레이드

--- backend/kid-service/src/main/java/com/helloword/kidservice/global/config/S3Config.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/global/config/S3Config.java
@@ -1,11 +1,9 @@
 package com.helloword.kidservice.global.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +18,11 @@ public class S3Config {
     private String secretKey;
 
     @Bean
-    public AmazonS3 getS3ClientBean() {
-        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.AP_NORTHEAST_2)
+    public S3Client getS3ClientBean() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .region(Region.AP_NORTHEAST_2)
                 .build();
     }
-
 }
