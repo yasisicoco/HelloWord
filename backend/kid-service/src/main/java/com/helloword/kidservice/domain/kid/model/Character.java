f3a0d53118c8d9e8883e96c2cfaacda720c931b5
박용빈 <pyb0718@naver.com>
2024-10-01 19:28:51 +0900

    Feat: 캐릭터 Enum 추가

---
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/model/Character.java
@@ -0,0 +1,12 @@
+package com.helloword.kidservice.domain.kid.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Character {
+    DUSTY("먼지쿤");
+
+    private String name;
+}
