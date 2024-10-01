66c2b7b679f8a5920f4a140dcb10fde3447375ee
강세현 <gedfgrdt0@gmail.com>
2024-10-01 18:08:32 +0900

    Feat: 단어 게임 결과 저장 관련 client 추가

---
+++ backend/game-service/src/main/java/com/helloword/gameservice/global/client/LogServiceClient.java
@@ -0,0 +1,14 @@
+package com.helloword.gameservice.global.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.helloword.gameservice.domain.game.dto.request.GameLogRequestDto;
+
+@FeignClient(name = "log-service")
+public interface LogServiceClient {
+
+	@PostMapping("/api/logs/internal")
+	void saveGameLog(@RequestBody GameLogRequestDto gameLogRequest);
+}
