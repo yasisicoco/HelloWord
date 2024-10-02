3254503e9171aa6b324a3ea07bc67253c22ac963
mrcsbin <mrcsbin@naver.com>
2024-10-02 12:46:19 +0900

    Feat: CORS Configuration 추가

---
+++ backend/api-gateway/src/main/java/com/helloword/apigateway/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.helloword.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
+    }
+}
