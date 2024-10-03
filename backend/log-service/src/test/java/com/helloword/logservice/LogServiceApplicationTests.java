ab6c883d94c082969bc56bf02fedcfc994b78e95
강세현 <gedfgrdt0@gmail.com>
2024-10-03 11:31:45 +0900

    Fix: LogService Test contextLoads 메서드 삭제

--- backend/log-service/src/test/java/com/helloword/logservice/LogServiceApplicationTests.java
+++ backend/log-service/src/test/java/com/helloword/logservice/LogServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.helloword.logservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LogServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
