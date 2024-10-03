226a40ef583de22cc306d95f95ca39f04e2f0e6c
박용빈 <pyb0718@naver.com>
2024-10-03 17:43:54 +0900

    Feat: 아이 게임 단어 조회 기능 추가

--- backend/word-service/src/main/java/com/helloword/wordservice/domain/word/service/WordServiceImpl.java
+++ backend/word-service/src/main/java/com/helloword/wordservice/domain/word/service/WordServiceImpl.java
@@ -1,8 +1,12 @@
 package com.helloword.wordservice.domain.word.service;
 
+import com.helloword.wordservice.domain.recognitionrate.model.RecognitionRate;
+import com.helloword.wordservice.domain.recognitionrate.repository.RecognitionRateRepository;
 import com.helloword.wordservice.domain.word.dto.response.GameWordResponseDto;
+import com.helloword.wordservice.domain.word.model.AnswerWordLog;
 import com.helloword.wordservice.domain.word.model.Word;
 import com.helloword.wordservice.domain.word.repository.WordRepository;
+import com.helloword.wordservice.global.client.KidServiceClient;
 import com.helloword.wordservice.global.client.ProbabilityServiceClient;
 import com.helloword.wordservice.global.exception.MainException;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static com.helloword.wordservice.global.exception.CustomException.NOT_FOUND;
@@ -18,29 +23,50 @@ import static com.helloword.wordservice.global.exception.CustomException.NOT_FOU
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class WordServiceImpl implements WordService {
     private final WordRepository wordRepository;
     private final ProbabilityServiceClient probabilityServiceClient;
+    private final KidServiceClient kidServiceClient;
+    private final RecognitionRateRepository recognitionRateRepository;
 
-    @Transactional(readOnly = true)
     @Override
     public List<Word> getWordListByIds(List<Long> ids) {
-        if(ids.isEmpty())
+        if (ids.isEmpty())
             throw new MainException(NOT_FOUND);
         return wordRepository.findAllById(ids);
     }
 
     @Override
     public GameWordResponseDto getWordListByKidId(Long kidId, Integer wordCount) {
-//        probabilityServiceClient.getAnswerWordLogs(kidId);
-        List<Word> words = wordRepository.findAll();
+
+        Integer kidMonth = kidServiceClient.getKidAgeById(kidId);
+        kidMonth = Math.min(Math.max(kidMonth, 18), 36);
+
+        List<RecognitionRate> recognitionRates = recognitionRateRepository.findAllByMonth(kidMonth);
+        List<AnswerWordLog> answerWordLogs = probabilityServiceClient.getAnswerWordLogs(kidId).answerWordLogs();
+
+        HashMap<Long, Float> recognitionRateHashMap = new HashMap<>();
+        for (RecognitionRate recognitionRate : recognitionRates) {
+            recognitionRateHashMap.put(recognitionRate.getId(), recognitionRate.getRate());
+        }
+        List<ComputedWord> resultWords = new ArrayList<>();
+        for (AnswerWordLog answerWordLog : answerWordLogs) {
+            Float rate = recognitionRateHashMap.get(answerWordLog.wordId());
+            Double computedScore = rate * answerWordLog.probability();
+            resultWords.add(new ComputedWord(answerWordLog.wordId(), computedScore));
+        }
+
+        resultWords.sort((o1, o2) -> Double.compare(o2.value, o1.value));
+
+        List<Word> words = wordRepository.findAllById(resultWords.stream().map(resultWord -> resultWord.id).toList());
         int count = Math.min(wordCount, words.size());
         List<Word> subListedWords = words.subList(0, count);
 
         List<GameWordResponseDto.WordDto> wordDtos = new ArrayList<>();
         for (Word word : subListedWords) {
             wordDtos.add(new GameWordResponseDto.WordDto(word.getId(), word.getWord(), word.getImageUrl(),
-                word.getVoiceUrl()));
+                    word.getVoiceUrl()));
         }
 
         return new GameWordResponseDto(wordDtos);
@@ -49,5 +75,15 @@ public class WordServiceImpl implements WordService {
     @Override
     public List<Word> getAllWords() {
         return wordRepository.findAll();
+    }
+
+    private static class ComputedWord {
+        Long id;
+        Double value;
+
+        public ComputedWord(Long id, Double value) {
+            this.id = id;
+            this.value = value;
+        }
     }
 }
