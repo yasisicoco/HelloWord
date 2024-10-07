62f821496b8d01aaf660ab28006f766e9312b5cf
강세현 <gedfgrdt0@gmail.com>
2024-10-07 16:31:20 +0900

    Feat: WordRepository 오답 단어 찾는 메서드 추가

--- backend/word-service/src/main/java/com/helloword/wordservice/domain/word/repository/WordRepository.java
+++ backend/word-service/src/main/java/com/helloword/wordservice/domain/word/repository/WordRepository.java
@@ -1,7 +1,14 @@
 package com.helloword.wordservice.domain.word.repository;
 
+import java.util.List;
+
 import com.helloword.wordservice.domain.word.model.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
+
+	@Query("SELECT w FROM Word w WHERE w.id NOT IN :excludeIds ORDER BY RAND() LIMIT :limit")
+	List<Word> findRandomWordsExcluding(@Param("excludeIds") List<Long> excludeIds, @Param("limit") int limit);
 }
