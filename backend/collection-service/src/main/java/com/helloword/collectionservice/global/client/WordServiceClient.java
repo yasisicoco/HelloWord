b9fdb507708de3682b4ed9d416c1e1879f1188f3
박용빈 <pyb0718@naver.com>
2024-10-08 02:44:38 +0900

    Fix: 모든단어조회 api 수정

--- backend/collection-service/src/main/java/com/helloword/collectionservice/global/client/WordServiceClient.java
+++ backend/collection-service/src/main/java/com/helloword/collectionservice/global/client/WordServiceClient.java
@@ -1,9 +1,8 @@
 package com.helloword.collectionservice.global.client;
 
+import com.helloword.collectionservice.domain.collection.dto.response.WordsResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.helloword.collectionservice.domain.collection.dto.response.WordsResponseDto;
 
 @FeignClient(name = "word-service")
 public interface WordServiceClient {
