fbc0a42c86759d4c5571004eb51c2175d87cedc0
박용빈 <pyb0718@naver.com>
2024-10-08 02:27:24 +0900

    Fix: 모든단어 조회 api url 수정

--- backend/collection-service/src/main/java/com/helloword/collectionservice/global/client/WordServiceClient.java
+++ backend/collection-service/src/main/java/com/helloword/collectionservice/global/client/WordServiceClient.java
@@ -8,6 +8,6 @@ import com.helloword.collectionservice.domain.collection.dto.response.WordsRespo
 @FeignClient(name = "word-service")
 public interface WordServiceClient {
 
-	@GetMapping("/internal/words")
+	@GetMapping("/internal/words/all")
 	WordsResponseDto getWords();
 }
