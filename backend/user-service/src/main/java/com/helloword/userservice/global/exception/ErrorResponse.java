4ad714bae602f099e9a08ab2f5cfdca201511bdb
박용빈 <pyb0718@naver.com>
2024-10-08 10:39:55 +0900

    Refactor: 예외처리 Refactor

---
+++ backend/user-service/src/main/java/com/helloword/userservice/global/exception/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.helloword.userservice.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private final boolean success = false;
+    private final int code;
+    private final String reason;
+
+
+    public ErrorResponse(int code, String reason) {
+        this.code = code;
+        this.reason = reason;
+    }
+}
