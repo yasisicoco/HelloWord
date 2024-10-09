222311d17feee59f3d46ef3cb95fc51038fa9853
강세현 <gedfgrdt0@gmail.com>
2024-10-09 21:27:08 +0900

    Feat: Kid 튜토리얼 여부 수정 internal 컨트롤러 api 추가

--- backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/controller/InternalKidController.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/controller/InternalKidController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Tag(name = "[내부]아이 API", description = "서버 내부에서 통신하는 아이에 대한 API 입니다.")
 public class InternalKidController {
+
     private final KidService kidService;
 
     @Operation(summary = "아이 개월 수 조회", description = "해당 아이의 개월 수를 조회합니다.")
@@ -25,4 +26,29 @@ public class InternalKidController {
     public void updateKidExp(@RequestBody KidExpUpdateRequestDto kidExpUpdateRequestDto) {
         kidService.increaseExperience(kidExpUpdateRequestDto);
     }
+
+    @Operation(summary = "아이 스피드게임 튜토리얼 여부 수정", description = "해당 아이의 스피드게임 튜토리얼 여부를 수정합니다.")
+    @PutMapping("/{kidId}/speed-game-tutorial")
+    public boolean changeIsSpeedGameTutorialCompleted(@RequestParam Long kidId) {
+        return kidService.changeIsSpeedGameTutorialCompleted(kidId);
+    }
+
+    @Operation(summary = "아이 말하기게임 튜토리얼 여부 수정", description = "해당 아이의 말하기게임 튜토리얼 여부를 수정합니다.")
+    @PutMapping("/{kidId}/speech-game-tutorial")
+    public boolean changeIsSpeechGameTutorialCompleted(@RequestParam Long kidId) {
+        return kidService.changeIsSpeechGameTutorialCompleted(kidId);
+    }
+
+    @Operation(summary = "아이 짝맞추기게임 튜토리얼 여부 수정", description = "해당 아이의 짝맞추기게임 튜토리얼 여부를 수정합니다.")
+    @PutMapping("/{kidId}/pair-game-tutorial")
+    public boolean changeIsPairGameTutorialCompleted(@RequestParam Long kidId) {
+        return kidService.changeIsPairGameTutorialCompleted(kidId);
+    }
+
+    @Operation(summary = "아이 동화게임 튜토리얼 여부 수정", description = "해당 아이의 동화게임 튜토리얼 여부를 수정합니다.")
+    @PutMapping("/{kidId}/fairytale-game-tutorial")
+    public boolean changeIsFairytaleGameTutorialCompleted(@RequestParam Long kidId) {
+        return kidService.changeIsFairytaleGameTutorialCompleted(kidId);
+    }
+
 }
