6924f61cf040d76a2fdbe535ac89133f873ba5d4
강세현 <gedfgrdt0@gmail.com>
2024-10-09 16:40:14 +0900

    Fix: Log관련 dto 수정

--- backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/dto/response/GameStatsResponseDto.java
+++ backend/kid-service/src/main/java/com/helloword/kidservice/domain/kid/dto/response/GameStatsResponseDto.java
@@ -6,12 +6,21 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class GameStatsResponseDto {
+
     private Double speedGameKidAverageCorrectRate;
     private Double speedGameGlobalAverageCorrectRate;
+    private Double speedGameKidAveragePlayTime;
+    private Double speedGameGlobalAveragePlayTime;
     private Double speechGameKidAverageCorrectRate;
     private Double speechGameGlobalAverageCorrectRate;
+    private Double speechGameKidAveragePlayTime;
+    private Double speechGameGlobalAveragePlayTime;
     private Double pairGameKidAverageCorrectRate;
     private Double pairGameGlobalAverageCorrectRate;
+    private Double pairGameKidAveragePlayTime;
+    private Double pairGameGlobalAveragePlayTime;
     private Double fairytaleGameKidAverageCorrectRate;
     private Double fairytaleGameGlobalAverageCorrectRate;
+    private Double fairytaleGameKidAveragePlayTime;
+    private Double fairytaleGameGlobalAveragePlayTime;
 }
